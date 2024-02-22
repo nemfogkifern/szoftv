67029765285ccbf8c90156e434638e08f54dd7ac
nemfogkifern <gyurinabercel@gmail.com>
2024-02-22 10:43:44 +0100

    Fibonacci datagriddel

--- Fibonacci/Form1.cs
+++ Fibonacci/Form1.cs
@@ -5,15 +5,23 @@ namespace Fibonacci
         public Form1()
         {
             InitializeComponent();
+            List<Elem> elemek = new List<Elem>();
             for (int i = 0; i < 10; i++)
             {
-                Button b = new Button();
-                b.Top = i * 30;
-                b.Text = Fibonacci(i).ToString();
-                Controls .Add(b);
+                //Button b = new Button();
+                //b.Top = i * 30;
+                //b.Text = Fibonacci(i).ToString();
+                //Controls.Add(b);
 
+                Elem újElem = new Elem();
+                újElem.Sorszám = i;
+                újElem.Érték = Fibonacci(i);
+
+                elemek.Add(újElem);
             }
+            dataGridView1.DataSource = elemek;
         }
+
 
         int Fibonacci(int n)
         {
@@ -21,5 +29,13 @@ namespace Fibonacci
             if (n == 1) return 1;
             return Fibonacci(n - 1) + Fibonacci(n - 2);
         }
+
+        class Elem
+        {
+            public int Sorszám { get; set; }
+            public long Érték { get; set; }
+        }
+
+
     }
 }
